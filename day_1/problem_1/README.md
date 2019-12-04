39539b51b40ffece54ab2982636c160c080c9611
goalaleo <goalaleo@gmail.com>
2019-12-04 21:11:38 +0200

    Move day 1 problem 1 to it’s own folder

---
+++ day_1/problem_1/README.md
@@ -0,0 +1,4 @@
+# How to run
+```
+ruby fuel.rb modules.txt
+```
--- day_1/problem_1/fuel.rb
+++ day_1/problem_1/fuel.rb
@@ -1,15 +1,9 @@
-require "pry"
-
 file_name = ARGV[0]
 
 mass_to_fuel_conversion = ->(module_mass) { (module_mass/3).floor - 2 }
 
-binding.pry
-
 total_fuel = File.readlines(file_name)
               .map {|line| line.strip.to_f } # convert each line to float mass
               .sum(&mass_to_fuel_conversion)
-
-binding.pry
 
 puts total_fuel
